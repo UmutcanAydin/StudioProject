722b7d554bb025927763b9ff2976722421ca6b28
Reyhan Eren <sumeyyereyhaneren@gmail.com>
2022-03-20 23:46:32 +0300

    Demo proje init

---
+++ Assets/Script/Weapons/AmoDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmoDisplay : MonoBehaviour
+{
+    public GameObject amoTextUI;
+    public int pistolCount;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        amoTextUI.GetComponent<Text>().text = "AMO:" + pistolCount;
+        
+    }
+}
---
+++ Assets/Script/Weapons/PistoFire.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistoFire : MonoBehaviour
+{
+    public GameObject blackPistol;
+    public GameObject muzzleFlash;
+    public AudioSource pistolShot;
+    public bool isFiring=false;
+    public float toTarget;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (isFiring==false)
+            {
+                StartCoroutine(FireThePistol());
+            }
+        }
+    }
+    IEnumerator FireThePistol()
+    {
+
+        isFiring = true;
+        toTarget = PlayerCasting.distanceFromTarget;
+        blackPistol.GetComponent<Animator>().Play("Fire_Pistol");
+        pistolShot.Play();
+        muzzleFlash.SetActive(true);
+        yield return new WaitForSeconds(0.05f);
+        muzzleFlash.SetActive(false);
+        yield return new WaitForSeconds(0.20f);
+        blackPistol.GetComponent<Animator>().Play("New State");
+        isFiring = false;
+    }
+}
