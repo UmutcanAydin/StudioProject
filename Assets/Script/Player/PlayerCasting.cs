9491251bb07c4bc82494ac54f5ce6815b82a02b9
Umutcan Aydin <umutcanaydin1998@gmail.com>
2022-04-08 01:41:45 +0300

    Projectile Eklendi

--- Assets/Script/Player/PlayerCasting.cs
+++ Assets/Script/Player/PlayerCasting.cs
@@ -16,6 +16,18 @@ public class PlayerCasting : MonoBehaviour
     public static float distanceFromTarget;
     public float toTarget;
 
+    [Header("Projectile Settings")]
+    public Transform projectilePosition;
+    public GameObject projectile;
+    public float shootCoolDown = 1f;
+    float coolDown;
+    public float force = 20f;
+    bool firing = false;
+    bool fired = false;
+    Coroutine fireRoutine;
+    Rigidbody projectileRGBD;
+    GameObject bullet;
+
     void Update()
     {
         //Deselect
@@ -52,11 +64,40 @@ public class PlayerCasting : MonoBehaviour
             }
         }
 
+        //Shooting
+        if(coolDown > 0) coolDown -= Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0) && coolDown <= 0)
+        {
+            firing = true;
+            fireRoutine = StartCoroutine(Fire());
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            firing = false;
+            if(fireRoutine != null) StopCoroutine(fireRoutine);
+            if(fired) coolDown = shootCoolDown;
+            fired = false;
+        }
+
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
         {
             toTarget = hit.distance;
             distanceFromTarget = hit.distance;            
+        }
+    }
+
+    IEnumerator Fire()
+    {
+        while (firing)
+        {
+            bullet = Instantiate(projectile, projectilePosition.position, projectilePosition.rotation);
+            projectileRGBD = bullet.GetComponent<Rigidbody>();
+
+            projectileRGBD.AddForce(bullet.transform.forward * force, ForceMode.Impulse);
+            fired = true;
+            yield return new WaitForSeconds(shootCoolDown);
         }
     }
 }
