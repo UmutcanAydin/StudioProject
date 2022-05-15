2afd1d487060c278bdb636351d00fd5366d40855
Umutcan Aydin <umutcanaydin1998@gmail.com>
2022-05-15 11:16:30 +0300

    Level3

--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -10,6 +10,12 @@ public class LevelManager : MonoBehaviour
     [HideInInspector] public int level1DeadEnemyCount = 0;
     bool level1Handled = false;
 
+    [Header("Level 3 Settings")]
+    public GameObject[] lvl3enemies;
+    public Door lvl3Door;
+    [HideInInspector] public int level3DeadEnemyCount = 0;
+    bool level3Handled = false;
+
     private void Update()
     {
         if (!level1Handled && level1DeadEnemyCount >= enemies.Length)
@@ -17,6 +23,12 @@ public class LevelManager : MonoBehaviour
             level1Handled = true;
             lvl1Door.keyFound = true;
             lvl1Door.Interact();
+        }
+        if (!level3Handled && level3DeadEnemyCount >= lvl3enemies.Length)
+        {
+            level3Handled = true;
+            lvl3Door.keyFound = true;
+            lvl3Door.Interact();
         }
     }
 }
